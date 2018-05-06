7903982557b6835decfc5405c373c752f05b8b0b
y-kisse <kichisecreative@gmail.com>
2018-05-06 17:11:39 +0900

    [fix] typo

--- backend/snippets/serializers.py
+++ backend/snippets/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
-from snippets.model import Snippet, LANGUAGE_CHOICE, STYLE_CHOICES
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
     linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICE, default='python')
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
 
     def create(self, validated_data):
@@ -21,8 +21,8 @@ class SnippetSerializer(serializers.Serializer):
         """
         instance.title = validated_data.get('title', instance.title)
         instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos' instance.linenos)
-        instance.language = validated_data.get('language' instance.language)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
         instance.style = validated_data.get('style', instance.style)
         instance.save()
 
