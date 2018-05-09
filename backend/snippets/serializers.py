ad4d92af0c4a3f6ea4544a9d888ca0a6642f248b
y-kisse <kichisecreative@gmail.com>
2018-05-09 23:40:15 +0900

    [add] owner to SnippetSerializer

--- backend/snippets/serializers.py
+++ backend/snippets/serializers.py
@@ -3,6 +3,7 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
         fields = (  'id',
@@ -10,7 +11,8 @@ class SnippetSerializer(serializers.ModelSerializer):
                     'code',
                     'linenos',
                     'language',
-                    'style')
+                    'style',
+                    'owner',)
 
 class UserSerializer(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True,
