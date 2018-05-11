5f18a431486c119ea6e48e221c5ab1812a97dd0f
y-kisse <kichisecreative@gmail.com>
2018-05-11 23:44:11 +0900

    [remove] loading needless modules.

--- backend/snippets/views.py
+++ backend/snippets/views.py
@@ -1,22 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from django.http import Http404
-from rest_framework.views import APIView
 from rest_framework import generics
-
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
-
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
-
-from rest_framework import generics
 
 class SnippetList(generics.ListCreateAPIView):
     """
