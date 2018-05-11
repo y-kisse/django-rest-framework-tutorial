d613de5cfc064d33413f8a2b5c6829d4a78e29d4
y-kisse <kichisecreative@gmail.com>
2018-05-12 00:08:41 +0900

    [add] highlight html view

--- backend/snippets/views.py
+++ backend/snippets/views.py
@@ -6,6 +6,17 @@ from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 class SnippetList(generics.ListCreateAPIView):
     """
@@ -27,6 +38,9 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
 
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -35,3 +49,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *arg, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
