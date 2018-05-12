ccb47ee20ce3f5958605ecc3c035016d090fac4d
y-kisse <kichisecreative@gmail.com>
2018-05-12 19:53:00 +0900

    [add] hiperlinkedAPI

--- backend/snippets/serializers.py
+++ backend/snippets/serializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
     class Meta:
         model = Snippet
         fields = (  'id',
@@ -13,11 +14,12 @@ class SnippetSerializer(serializers.ModelSerializer):
                     'language',
                     'style',
                     'owner',
-                    )
+                    'highlight')
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True,
-                                                  queryset=Snippet.objects.all())
+    snippets = serializers.HyperlinkedRelatedField(many=True,
+                                                   view_name='snippet-detail',
+                                                   read_only=True)
 
     class Meta:
         model = User
--- backend/snippets/urls.py
+++ backend/snippets/urls.py
@@ -4,11 +4,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf.urls import include
 
 urlpatterns = [
-    url(r'^snippets/$', views.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-    url(r'snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
+    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
+    url(r'snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
     url(r'^$', views.api_root),
 ]
 
