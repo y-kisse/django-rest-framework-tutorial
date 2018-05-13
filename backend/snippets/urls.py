9f67a640eb843182c27aec9aafe70fc2c4f4c5f4
y-kisse <kichisecreative@gmail.com>
2018-05-13 18:48:56 +0900

    [add] schema view

--- backend/snippets/urls.py
+++ backend/snippets/urls.py
@@ -2,6 +2,7 @@ from snippets.views import SnippetViewSet, UserViewSet, api_root
 from rest_framework import renderers
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 snippet_list = SnippetViewSet.as_view({
     'get': 'list',
@@ -31,6 +32,9 @@ router = DefaultRouter()
 router.register(r'snippets', SnippetViewSet)
 router.register(r'users', UserViewSet)
 
+schema_view = get_schema_view(title='Pastebin API')
+
 urlpatterns = [
-    url(r'^', include(router.urls))
+    url(r'^', include(router.urls)),
+    url(r'^schemas/$', schema_view),
 ]
